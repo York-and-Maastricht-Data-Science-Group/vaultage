ec2f9480ea6a220bcc980dafc8cfd8ae0a71c224
alfonsodelavega <alfonso.delavega@tuta.io>
2020-05-28 17:03:04 +0100

    Pollen: Fix problem with participants traversing
    Now any number of participants works.
    
    Also, there are more informative messages along the execution, and the
    final value (the accumulated - the fake one) is also displayed.

--- org.vaultage.demo.pollen/src/org/vaultage/demo/pollen/app/cli/SimpleClient.java
+++ org.vaultage.demo.pollen/src/org/vaultage/demo/pollen/app/cli/SimpleClient.java
@@ -1,8 +1,11 @@
 package org.vaultage.demo.pollen.app.cli;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.apache.activemq.ActiveMQConnection;
 import org.vaultage.core.Vaultage;
@@ -11,24 +14,35 @@ import org.vaultage.core.VaultageServer;
 import org.vaultage.demo.pollen.app.SendNumberPollRequestHandler;
 import org.vaultage.demo.pollen.app.SendNumberPollResponseHandler;
 import org.vaultage.demo.pollen.app.User;
-import org.vaultage.demo.pollen.data.PollRepository;
+import org.vaultage.demo.pollen.custom.CustomRemoteUser;
 import org.vaultage.demo.pollen.gen.NumberPoll;
-import org.vaultage.demo.pollen.gen.RemoteRequester;
 import org.vaultage.demo.pollen.util.PollenUtil;
 
 /***
- * NOTE: 
+ * NOTE:
  * So far, this demo can only run with the REAL ActiveMQ server. It will NOT work if we use the VIRTUAL ActiveMQ.
- * To run this demo, start the ActiveMQ server, and then run this demo. 
- * Also, it can only accommodate 3 users. Charlie and Bob should be started and listening/subscribing first before
- * Alice starts a polling. Otherwise, Charlie and Bob cannot get Alice's poll.
- * I will figure out this later.       
+ * To run this demo, start the ActiveMQ server, and then run this demo.
+ * 
+ * It can accomodate any number of participants as long as the users responding
+ * polls are started and listening (option 2) before the person starting the poll does so
+ * 
+ * Note: delete the contents of the keys directory before starting any test unless
+ * using the same number of participants as files are in the directory
+ * (fixes problems when certain participant is not started but its file key is found)
+ * 
+ * I will figure out this later.
  */
 
 public class SimpleClient {
 
 	public static final int STARTING_POLL = 0;
 	public static final int ANSWERING_POLLS = 1;
+
+	public static final double MIN_RANDOM = 1000000;
+	public static final double MAX_RANDOM = 10000000;
+
+	// TODO: treat this storage in a more elegant way (good enough for the demo)
+	static Map<String, Double> poll2fakeValue = new HashMap<>();
 
 	public static void main(String[] args) throws Exception {
 
@@ -45,7 +59,8 @@ public class SimpleClient {
 		// set the address of Vaultage server
 		String address = ActiveMQConnection.DEFAULT_BROKER_URL;
 //		String address = "vm://localhost";
-		VaultageServer pollen = new VaultageServer(address);
+		VaultageServer pollenServer = new VaultageServer(address);
+		user.register(pollenServer);
 
 		// set handlers
 		user.setSendNumberPollRequestBaseHandler(new SendNumberPollRequestHandler(scan) {
@@ -57,31 +72,51 @@ public class SimpleClient {
 				NumberPoll poll = Vaultage.Gson.fromJson(json, NumberPoll.class);
 
 				if (localVault.getPublicKey().equals(poll.getOriginator())) {
-					double myFakeSalary = 1;
-					result = myFakeSalary;
+
+					System.out.println("The poll request is back to me as the originator");
+					
+					double fakeValue = ThreadLocalRandom.current().nextDouble(MIN_RANDOM, MAX_RANDOM);
+					result = fakeValue;
+					poll2fakeValue.put(poll.getId(), fakeValue);
+
+
+					System.out.printf("Sending fake value: %f\n", fakeValue);
+
 					return (double) result;
+
 				} else {
 					localVault.getPolls().put(poll.getId(), poll);
 
+					System.out.println("\nNew poll received!");
+					
+					List<String> participants = poll.getParticipants();
+					int index = participants.indexOf(localVault.getPublicKey());
+					
+					System.out.printf("I am participant %d\n\n", index + 1);
+
 					PollenCLI.displayPoll(poll);
+					double answer = PollenCLI.readValue(scan);
 
-					double total = 0;
-
-					String mySalary = "";
-
-					System.out.print("Answer: ");
-					if (scan.hasNext())
-						mySalary = scan.next();
-
-					for (String publicKey : poll.getParticipants()) {
-						if (!publicKey.equals(localVault.getPublicKey())
-								&& !publicKey.equals(senderMessage.getFrom())) {
-							total = total + (new RemoteRequester(localVault.getVaultageServer(), localVault))
-									.sendNumberPoll(publicKey, poll);
-						}
+					String nextParticipant;
+					if (index + 1 < participants.size()) {
+						nextParticipant = participants.get(index + 1);
+						System.out.println("\nSending to next participant");
 					}
-
-					result = total + Double.valueOf(mySalary);
+					else {
+						// we were the last participant. back to originator
+						nextParticipant = poll.getOriginator();
+						System.out.println("\nSending back to originator");
+					}
+					double currentTotal =
+							(new CustomRemoteUser(
+									localVault.getVaultageServer(),
+									localVault,
+									nextParticipant))
+							.sendNumberPoll(poll);
+					
+					System.out.printf("\nCurrent accumulated value: %f\n", currentTotal);
+					
+					result = currentTotal + answer;
 					return (double) result;
 				}
 			}
@@ -96,12 +131,10 @@ public class SimpleClient {
 		switch (choice) {
 		case STARTING_POLL:
 			System.out.println("Starting a poll");
-			startAPoll(user, pollen);
+			startAPoll(scan, user, pollenServer);
 			break;
 		case ANSWERING_POLLS:
-			System.out.println("Answering polls");
-			System.out.println("Waiting for incoming polls ...");
-			answerAPoll(user, pollen);
+			System.out.println("Waiting for incoming polls");
 			break;
 		}
 		
@@ -112,34 +145,26 @@ public class SimpleClient {
 		scan.close();
 	}
 
-	private static void startAPoll(User user, VaultageServer vaultageServer) throws Exception {
-		List<String> participants = PollenUtil.getParticipants();
-		System.out.println("Create salary poll ...");
-		NumberPoll poll = PollRepository.createSalaryPoll();
+	private static void startAPoll(Scanner scan, User user, VaultageServer vaultageServer) throws Exception {
+
+		NumberPoll poll = PollenCLI.createNumberPoll(scan);
 		poll.setOriginator(user.getPublicKey());
+		// participants stores the list of public keys
+		List<String> participants = PollenUtil.getParticipants(user.getName());
 		poll.setParticipants(participants);
 
 		if (participants.size() > 0) {
-			// connect to activemq
-			user.register(vaultageServer);
-
-			int userIndex = participants.indexOf(user.getPublicKey());
-			int nextUserIndex = userIndex + 1;
-			if (nextUserIndex == participants.size()) {
-				nextUserIndex = 0;
-			}
-			String nextParticipant = participants.get(nextUserIndex);
-			System.out.println("Send the poll to " + nextParticipant + " ...");
-			RemoteRequester remoteRequester = new RemoteRequester(vaultageServer, user);
-			double total = remoteRequester.sendNumberPoll(nextParticipant, poll);
+			String firstParticipant = participants.get(0);
+			System.out.println("Sending the poll to " + firstParticipant);
+			CustomRemoteUser remoteUser =
+					new CustomRemoteUser(vaultageServer, user, firstParticipant);
+			double total = remoteUser.sendNumberPoll(poll);
 			System.out.println("Fake total: " + total);
+			double randomValue = poll2fakeValue.get(poll.getId());
+			System.out.println("Real total: " + (total - randomValue));
 		} else {
 			System.out.println("No participants available. Please run another User to share its public key.");
 		}
-	}
-
-	private static void answerAPoll(User user, VaultageServer vaultageServer) throws Exception {
-		user.register(vaultageServer);
 	}
 
 }
--- org.vaultage.demo.pollen/src/org/vaultage/demo/pollen/util/PollenUtil.java
+++ org.vaultage.demo.pollen/src/org/vaultage/demo/pollen/util/PollenUtil.java
@@ -6,11 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.vaultage.demo.pollen.app.User;
 
@@ -33,14 +30,20 @@ public class PollenUtil {
 		Path path = Paths.get(pathString);
 		return new String(Files.readAllBytes(path));
 	}
-	
-	 public static List<String> getParticipants() throws IOException{
-		 Path path = Paths.get(PATH);
-		 List<String> names = Files.list(path).map(p -> p.getFileName().toString()).collect(Collectors.toList());
-		 List<String> participants = new ArrayList<>();
-		 for (String name : names) {
-			 participants.add(getPublicKey(name));
-		 }
-		 return participants;
-	 }
+
+	/**
+	 * We currently use everyone as participant but the poll originator
+	 */
+	public static List<String> getParticipants(String pollOriginator) throws IOException {
+		Path path = Paths.get(PATH);
+		List<String> names = Files.list(path)
+				.map(p -> p.getFileName().toString())
+				.collect(Collectors.toList());
+		names.remove(pollOriginator);
+		List<String> participants = new ArrayList<>();
+		for (String name : names) {
+			participants.add(getPublicKey(name));
+		}
+		return participants;
+	}
 }
