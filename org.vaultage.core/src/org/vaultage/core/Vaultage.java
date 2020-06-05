179f8fbe1896da5a0657859788440a4c5315147f
Alfa Yohannis <ary506@york.ac.uk>
2020-06-05 14:43:06 +0100

    Use MessageListenser instead of using custom thread management
    Use MessageListenser instead of using custom thread management

--- org.vaultage.core/src/org/vaultage/core/Vaultage.java
+++ org.vaultage.core/src/org/vaultage/core/Vaultage.java
@@ -1,6 +1,6 @@
 package org.vaultage.core;
 
-import java.security.KeyFactory;
+import java.net.URI;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.util.Base64;
@@ -12,23 +12,29 @@ import java.util.Set;
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.jms.Topic;
 
+import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
 import org.vaultage.util.VaultageEncryption;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /***
- * The main class that is responsible to listen, send, and receive messages to
- * and from Apache ActiveMQ server.
+ * The main class that is responsible to connect, disconnect, listen, send, and
+ * receive messages to and from Apache ActiveMQ server.
  * 
- * @author Ryano
+ * @author Alfa Yohannis
  *
  */
 public class Vaultage {
@@ -39,8 +45,7 @@ public class Vaultage {
 	private ActiveMQConnectionFactory connectionFactory;
 	private Connection connection;
 	private Session session;
-	private boolean isListening;
-	private Set<VaultageHandler> threads;
+	private Set<VaultageHandler> threads = new HashSet<VaultageHandler>();
 	private Set<String> expectedReplyTokens = new HashSet<>();
 
 	public Set<String> getExpectedReplyTokens() {
@@ -52,12 +57,18 @@ public class Vaultage {
 	}
 
 	/***
+	 * Test or demo this Vaultage class
 	 * 
 	 * @param args
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
 
+		final int SLEEP_TIME = 500;
+
+		BrokerService broker = BrokerFactory.createBroker(new URI("broker:(tcp://localhost:61616)"));
+		broker.start();
+		
 		// encryption
 		KeyPair receiverKeyPair;
 		KeyPair senderKeyPair;
@@ -67,217 +78,197 @@ public class Vaultage {
 		receiverKeyPair = keyPairGen.generateKeyPair();
 		senderKeyPair = keyPairGen.generateKeyPair();
 
-//		String address = ActiveMQConnection.DEFAULT_BROKER_URL;
-		String address = "vm://localhost";
+		String receiverPublicKey = Base64.getEncoder().encodeToString(receiverKeyPair.getPublic().getEncoded());
+		String receiverPrivateKey = Base64.getEncoder().encodeToString(receiverKeyPair.getPrivate().getEncoded());
+
+		String senderPublicKey = Base64.getEncoder().encodeToString(senderKeyPair.getPublic().getEncoded());
+		String senderPrivateKey = Base64.getEncoder().encodeToString(senderKeyPair.getPrivate().getEncoded());
+
+		String address = ActiveMQConnection.DEFAULT_BROKER_URL;
+//		String address = "vm://localhost";
 		Vaultage v1 = new Vaultage();
 		v1.connect(address);
 
 		Vaultage v2 = new Vaultage();
 		v2.connect(address);
 
+		Thread.sleep(SLEEP_TIME);
+
 		HashMap<String, VaultageHandler> handlers = new HashMap<String, VaultageHandler>();
 		handlers.put(VaultageHandler.class.getName(), new VaultageHandler() {
 			public void run() {
 				System.out.println("Received message: " + this.message.getValue("value"));
-
 			}
 		});
-		Thread t2 = v2.subscribe("bob",
-				Base64.getEncoder().encodeToString(receiverKeyPair.getPrivate().getEncoded()), handlers);
+
+		v2.subscribe(receiverPublicKey, receiverPrivateKey, handlers);
+
+		Thread.sleep(SLEEP_TIME);
 
 		VaultageMessage message = new VaultageMessage();
-		message.setFrom("alice");
-		message.setTo("bob");
+		message.setFrom(senderPublicKey);
+		message.setTo(receiverPublicKey);
 		message.setOperation(VaultageHandler.class.getName());
 		message.putValue("value", "Hello World!");
-		Thread t1 = v1.sendMessage(message.getTo(),
-				Base64.getEncoder().encodeToString(senderKeyPair.getPublic().getEncoded()),
-				Base64.getEncoder().encodeToString(senderKeyPair.getPrivate().getEncoded()), message);
+		v1.sendMessage(receiverPublicKey, senderPublicKey, senderPrivateKey, message);
 
-//		synchronized (t1) {
-//			t1.wait();
-//		}
-
-//		synchronized (t2) {
-//			t2.wait();
-//		}
-
-		Thread.sleep(1000);
+		Thread.sleep(SLEEP_TIME);
 
 		v1.disconnect();
 		v2.disconnect();
+
+		broker.stop();
 		System.out.println("Finished!");
 	}
 
 	/***
+	 * A method to send a message to ActiveMQ broker
 	 * 
+	 * @param topicId
+	 * @param senderPublicKey
+	 * @param senderPrivateKey
+	 * @param message
+	 * @throws InterruptedException
 	 */
-	public Vaultage() {
-		threads = new HashSet<VaultageHandler>();
-	}
+	public void sendMessage(String topicId, String senderPublicKey, String senderPrivateKey, VaultageMessage message)
+			throws InterruptedException {
+		try {
+			// Create the destination (Topic or Queue)
+			Topic destination = session.createTopic(topicId);
 
-	public Thread sendMessage(String topicId, String senderPublicKey, String senderPrivateKey,
-			VaultageMessage message) throws InterruptedException {
-		Thread brokerThread = new Producer(topicId, senderPublicKey, senderPrivateKey, message);
-		brokerThread.setDaemon(false);
-		brokerThread.setName(brokerThread.getClass().getName() + "-" + topicId);
-		brokerThread.start();
-		return brokerThread;
-	}
+			// Create a MessageProducer from the Session to the Topic or Queue
+			MessageProducer producer = session.createProducer(destination);
+			producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 
-	public Thread subscribe(String topicId, String receiverPrivateKey, Map<String, VaultageHandler> handlers) throws InterruptedException {
-		this.isListening = true;
-		Thread brokerThread = new Consumer(topicId, receiverPrivateKey, handlers);
-		brokerThread.setDaemon(true);
-		brokerThread.setName(brokerThread.getClass().getName() + "-" + topicId);
-		brokerThread.start();
-		return brokerThread;
-	}
+			// Create a message
+			String text = Gson.toJson(message).trim();
 
-	public boolean connect(String address) throws Exception {
-		this.address = address;
-		connectionFactory = new ActiveMQConnectionFactory(this.address);
-		connection = connectionFactory.createConnection();
-		connection.start();
-		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		return true;
-	}
+			// encrypt message
+			String encryptedMessage = VaultageEncryption.doubleEncrypt(text, topicId, senderPrivateKey).trim();
 
-	public void disconnect() throws Exception {
-		this.unsubscribe();
+			TextMessage m = session.createTextMessage(senderPublicKey + encryptedMessage);
 
-		for (VaultageHandler h : threads) {
-			if (h.isAlive()) {
-				h.interrupt();
-			}
-		}
-//		session.rollback();
-		session.close();
-		connection.stop();
-		connection.close();
-	}
+			producer.send(m);
+			
+			System.out.println("Send to: " + topicId);
+			
+			expectedReplyTokens.add(message.getToken());
+//			System.out.println("SENT MESSAGE: " + topicId + "\n" + text);
 
-	private class Producer extends Thread {
-
-		String topicId;
-		String senderPublicKey;
-		String senderPrivateKey;
-		VaultageMessage message;
-		String text;
-
-		public Producer(String topicId, String senderPublicKey, String senderPrivateKey, VaultageMessage message) {
-			this.topicId = topicId;
-			this.senderPublicKey = senderPublicKey;
-			this.senderPrivateKey = senderPrivateKey;
-			this.message = message;
-		}
-
-		public void run() {
-			try {
-//				System.out.println("Send to: " + topicId);
-
-				// Create the destination (Topic or Queue)
-				Destination destination = session.createTopic(topicId);
-
-				// Create a MessageProducer from the Session to the Topic or Queue
-				MessageProducer producer = session.createProducer(destination);
-				producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-
-				// Create a message
-				text = Gson.toJson(message).trim();
-
-				// encrypt message
-				String encryptedMessage = VaultageEncryption.doubleEncrypt(text, topicId, senderPrivateKey).trim();
-
-				TextMessage message = session.createTextMessage(this.senderPublicKey + encryptedMessage);
-
-				producer.send(message);
-
-				expectedReplyTokens.add(this.message.getToken());
-//				System.out.println("SENT MESSAGE: " + topicId + "\n" + text);
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
-	private class Consumer extends Thread {
-		private String topicId;
-		private String senderPublicKey;
-		private String receiverPrivateKey;
-		private String json;
-		private Map<String, VaultageHandler> handlers;
+	/***
+	 * A method to listen to ActiveMQ broker for incoming messages
+	 * 
+	 * @param topicId
+	 * @param receiverPrivateKey
+	 * @param handlers
+	 * @throws InterruptedException
+	 */
+	public void subscribe(String topicId, String receiverPrivateKey, Map<String, VaultageHandler> handlers)
+			throws InterruptedException {
+		try {
+			// Create the destination (Topic or Queue)
+			Topic destination = session.createTopic(topicId);
 
-		public Consumer(String topicId, String receiverPrivateKey, Map<String, VaultageHandler> handlers) {
-			this.topicId = topicId;
-			this.handlers = handlers;
-			this.receiverPrivateKey = receiverPrivateKey;
-		}
+			// Create a MessageConsumer from the Session to the Topic or Queue
+			MessageConsumer consumer = session.createConsumer(destination);
 
-		public void run() {
-			try {
+			consumer.setMessageListener(new MessageListener() {
 
-				// Create the destination (Topic or Queue)
-//				System.out.println(session + " - " + queueId);
-				Destination destination = session.createTopic(topicId);
-
-				// Create a MessageConsumer from the Session to the Topic or Queue
-				MessageConsumer consumer = session.createConsumer(destination);
-
-//				System.out.println("Listening to " + topicId);
-				// listen forever until listening is turned off
-				while (isListening) {
-
-					// Wait for a message, 0 means listen forever
-					Message message = consumer.receive(0);
-
-					if (message != null) {
-
+				@Override
+				public void onMessage(Message message) {
+					try {
 						TextMessage textMessage = (TextMessage) message;
 
 						String mergedMessage = textMessage.getText();
-						this.senderPublicKey = mergedMessage.substring(0, 128);
-//						System.out.println("X: " + this.senderPublicKey);
+						String senderPublicKey = mergedMessage.substring(0, 128);
 						String encryptedMessage = mergedMessage.substring(128, mergedMessage.length());
 
-//						System.out.println("M: " + encryptedMessage);
-//						System.out.println("B:" + this.senderPublicKey.getBytes().length + ", " + encryptedMessage.getBytes().length);
-//						System.out.println("S:" + this.senderPublicKey.length() + ", " + encryptedMessage.length());
-//						System.out.println("A:" + textMessage.getText().length());
-
-						json = VaultageEncryption.doubleDecrypt(encryptedMessage, senderPublicKey, receiverPrivateKey);
+						String json = VaultageEncryption.doubleDecrypt(encryptedMessage, senderPublicKey,
+								receiverPrivateKey);
 
 //						System.out.println("RECEIVED MESSAGE: " + topicId + "\n" + json);
 
 						VaultageMessage vaultageMessage = Gson.fromJson(json, VaultageMessage.class);
 						String operation = vaultageMessage.getOperation();
-						
+
 						VaultageHandler handler = handlers.get(operation);
 						if (handler != null && !handler.isAlive()) {
 							threads.add(handler);
 //							System.out.println("Run: " + handler.getName());
 							handler.execute(topicId, vaultageMessage);
-						} else {
-//							System.out.println();
 						}
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
 				}
+			});
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			 System.out.println("Listening to: " + topicId);
+//			// Wait for a message, 0 means listen forever
+//			Message message = consumer.receive(0);
+
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
-	public void unsubscribe() {
-		this.isListening = false;
+	/***
+	 * A method to connect to an ActiveMQ broker
+	 * 
+	 * @param address
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean connect(String address) throws Exception {
+		try {
+			this.address = address;
+			connectionFactory = new ActiveMQConnectionFactory(this.address);
+			connection = connectionFactory.createConnection();
+			connection.start();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 
+	/***
+	 * A method to disconnect from an ActiveMQ broker
+	 * 
+	 * @throws Exception
+	 */
+	public void disconnect() throws Exception {
+		for (VaultageHandler h : threads) {
+			if (h.isAlive()) {
+				h.interrupt();
+			}
+		}
+		session.close();
+		connection.stop();
+		connection.close();
+	}
+
+	/***
+	 * Get the address of the ActiveMQ broker
+	 * 
+	 * @param address
+	 */
 	public String getAddress() {
 		return address;
 	}
 
+	/***
+	 * Set the address of the ActiveMQ broker
+	 * 
+	 * @param address
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
