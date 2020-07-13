30796e2c609d7a5654175725136b616bc8553e93
alfonsodelavega <alfonso.delavega@tuta.io>
2020-07-13 17:55:42 +0200

    Include synthetiser app

---
+++ org.vaultage.demo.synthesiser/src/org/vaultage/demo/synthesiser/Worker.java
@@ -0,0 +1,38 @@
+package org.vaultage.demo.synthesiser;
+
+public class Worker extends WorkerBase {
+
+	private int currentValue;
+	private int completedValue;
+
+	public Worker() throws Exception {
+		super();
+		currentValue = 0;
+	}
+
+	public void sendOperation(String workerPublicKey) throws Exception {
+		RemoteWorker worker = new RemoteWorker(this, workerPublicKey);
+		synchronized (this.getIncrementResponseHandler()) {
+			worker.increment(currentValue);
+			this.getIncrementResponseHandler().wait();
+		}
+	}
+
+	@Override
+	public void increment(Worker requesterWorker, String requestToken, Integer number) throws Exception {
+		RemoteWorker remote = new RemoteWorker(this, requesterWorker.getPublicKey());
+		remote.respondToIncrement(number + 1, requestToken);
+	}
+
+	public boolean isWorkComplete() {
+		return currentValue >= completedValue;
+	}
+
+	public void setCurrentValue(int currentValue) {
+		this.currentValue = currentValue;
+	}
+
+	public void setCompletedValue(int completedValue) {
+		this.completedValue = completedValue;
+	}
+}
