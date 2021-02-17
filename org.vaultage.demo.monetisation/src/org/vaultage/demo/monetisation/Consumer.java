4b3b41149f002c07fb4bae3fc073d529847055a1
Alfa York <alfa.yohannis@york.ac.uk>
2021-02-17 18:47:14 +0000

    Upadate getResponseHandler to getOperationResponseHandler in org.vaultage.generator/generator/ResponseMessageHandler.egl

---
+++ org.vaultage.demo.monetisation/src/org/vaultage/demo/monetisation/Consumer.java
@@ -0,0 +1,32 @@
+package org.vaultage.demo.monetisation;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.vaultage.wallet.PaymentInformation;
+
+// import org.vaultage.demo.monetisation.Questionnaire;
+// import org.vaultage.demo.monetisation.Content;
+// import org.vaultage.demo.monetisation.ConsumerBase;
+
+public class Consumer extends ConsumerBase {
+
+	public Consumer() throws Exception {
+		super();
+	}
+	
+	public Consumer(String address, int port) throws Exception {
+		super(address, port);
+	}
+	
+	// getter
+	
+	// setter
+	
+	// operations
+	
+}
