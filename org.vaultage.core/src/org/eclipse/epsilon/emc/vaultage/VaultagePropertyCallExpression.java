6ed9459a7ba8d4667f7eaf04c97b3aee4b1306ae
Alfa Yohannis <alfa.yohannis@york.ac.uk>
2021-08-02 04:02:32 +0100

    Add some comments

--- org.vaultage.core/src/org/eclipse/epsilon/emc/vaultage/VaultagePropertyCallExpression.java
+++ org.vaultage.core/src/org/eclipse/epsilon/emc/vaultage/VaultagePropertyCallExpression.java
@@ -1,38 +1,34 @@
 package org.eclipse.epsilon.emc.vaultage;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.eclipse.epsilon.common.module.IModule;
 import org.eclipse.epsilon.common.module.ModuleElement;
 import org.eclipse.epsilon.eol.EolModule;
+import org.eclipse.epsilon.eol.dom.Expression;
 import org.eclipse.epsilon.eol.dom.FirstOrderOperationCallExpression;
 import org.eclipse.epsilon.eol.dom.NameExpression;
 import org.eclipse.epsilon.eol.dom.PropertyCallExpression;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.context.IEolContext;
-import org.eclipse.epsilon.eol.parse.EolUnparser;
 import org.eclipse.epsilon.eol.types.EolMap;
 import org.vaultage.core.RemoteVault;
-import org.vaultage.core.Vault;
 
 /***
- * This class converts property call to calling an operation. For example:
- * x.posts.collect(...) to x.getPosts().collect(...).
+ * This class is responsible to automatically infer vaults if they are local
+ * vaults and remote vaults. If a vault is a remote vault then requests will be
+ * sent to the remote vaults to retrieve the data. If the vault is a local vault
+ * then the data will be retrieved locally. For example:
+ * 
+ * localVault.posts -> this will retrieve posts from the local vault
+ * remoteVault.posts -> this will send request to the remote vault by sending
+ * sub expressions to the remote vault to be executed locally in the remote
+ * vault.
  * 
  * @author Alfa Yohannis
  *
  */
 public class VaultagePropertyCallExpression extends PropertyCallExpression {
 
-	/***
-	 * The method replace
-	 */
 	@Override
 	public Object execute(Object source, NameExpression propertyNameExpression, IEolContext context)
 			throws EolRuntimeException {
@@ -45,19 +41,47 @@ public class VaultagePropertyCallExpression extends PropertyCallExpression {
 
 			if (op != null) {
 
+				/**
+				 * Module has to be unparsed in its entirety to initialise all required objects.
+				 * Otherwise, when parsing only certain expressions, it would fail, since the
+				 * all preceding/required objects haven't been initialised.
+				 */
 				EolModule module = (EolModule) context.getModule();
 				VaultageUnparser unparser = new VaultageUnparser();
-				String code = unparser.unparse(module);
-//				ModuleElement moduleElement =  propertyNameExpression;
-				ModuleElement moduleElement = this;
-//				ModuleElement moduleElement = this.getParent();
+				unparser.unparse(module);
 
+				/***
+				 * if the the target expression is the current expression (e.g., in
+				 * remoteVault.posts.select(), the posts' parent expression is select(), but the
+				 * select() operation's target expression is the posts since the results are
+				 * returned back to the posts. operation's target is posts). In this case, we
+				 * take select() as the expression to be unparsed as a whole; this will also
+				 * unparsed the remoteVault and posts. Otherwise, the current expression will be
+				 * unparsed (e.g., remoteVault.posts). The post will be unparsed as a whole;
+				 * remoteVault is also included in the unparsed process.
+				 */
+				ModuleElement moduleElement = null;
+				Expression parentTargetExpression = ((FirstOrderOperationCallExpression) this.getParent())
+						.getTargetExpression();
+				if (parentTargetExpression.equals(this)) {
+					moduleElement = this.getParent();
+				} else {
+					moduleElement = this;
+				}
+
+				/***
+				 * Construct a query string (EOL script) and treat all remote vault name
+				 * expressions as a local vault.
+				 */
 				Object target = ((NameExpression) this.getTargetExpression()).getName();
 				String localVaultClass = ((RemoteVault) source).getLocalVault().getClass().getSimpleName();
 				String statement = unparser.unparse(moduleElement);
 				statement = "var " + target + " = " + localVaultClass + ".all.first;\n return " + statement + ";";
 
 				try {
+					/***
+					 * Call the Vault's Query operation
+					 */
 					String query = statement;
 					String queryMethod = "query";
 					EolMap<?, ?> map = new EolMap<>();
